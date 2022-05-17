8180313dacfee2c8975f2e86685b0d058b49c9a6
jakeisadev <starowicz@protonmail.com>
2022-05-17 09:20:23 -0400

    additions to background and input

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 //Access Key: _qiFC3j_JuFn4ZjO7ks1Z4h3dUDevonoxangBW2eXZo
 //Secret Key: CkYoJ9j7ZtpH2NK3JniD48cpmoVMV6QNOKP5CUxASRU
 
-//Look up how to make HTML/CSS cards to display the data: Disp 3 items in a row (or 4)
-//Also how to display from forEach itself
 const dispImage = (e) => {
+  //Don't let the form refresh the page as its default action once the user presses "Enter".
 e.preventDefault();
 let data = document.getElementById('data').value;
+//Data that will be retrieved is the value returned from the input field.
 document.getElementById('data').value = '';
+//When data is entered by the user, clear the input field for a new query.
 //End Point: 
 let url = `https://api.unsplash.com/search/photos?page=1&query=${data}&client_id=_qiFC3j_JuFn4ZjO7ks1Z4h3dUDevonoxangBW2eXZo`
-
+let output = '' //Variable to hold the dynamic HTML content
 fetch(url)
 .then(res => res.json())
 .then((data) => {
-    // console.log(data);
     data.results.forEach((item) => {
-        console.log(item)
-    }) 
+      //forEach item within the fetched API, output a card containing the image,
+      //description, and amount of likes that it has.
+
+      //We do this by letting JS dynamically enter HTML depending on the data retrieved. 
+        output += `
+        <div class="card">
+        <div id="picture">
+        <img src="${item.urls.regular}" alt="${item.description ?? ''}">
+        </div>
+        <div id="container">
+           <p>Liked By: ${item.likes}</p>
+        </div>
+      </div>`
+    })
+    //Finalizes the data into the "card-container" div within the index.hmtml
+    document.getElementById("card-container").innerHTML = output;
 })
 }
+
