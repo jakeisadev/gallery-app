2b8b69cab7126267c1e005436398cf4e55633cb3
jakeisadev <starowicz@protonmail.com>
2022-05-19 09:19:41 -0400

    updates

--- script.js
+++ script.js
@@ -34,3 +34,27 @@ fetch(url)
 })
 }
 
+const onLoadImage = () => { 
+let url = `https://api.unsplash.com/search/photos?page=1&query=cat&client_id=_qiFC3j_JuFn4ZjO7ks1Z4h3dUDevonoxangBW2eXZo`
+fetch(url)
+.then(res => res.json())
+.then((data) => {
+    data.results.forEach((item) => {
+      //forEach item within the fetched API, output a card containing the image,
+      //description, and amount of likes that it has.
+
+      //We do this by letting JS dynamically enter HTML depending on the data retrieved. 
+        output += `
+        <div class="card">
+        <div id="picture">
+        <img src="${item.urls.regular}" alt="${item.description ?? ''}">
+        </div>
+        <div id="container">
+           <p>Liked By: ${item.likes}</p>
+        </div>
+      </div>`
+    })
+    //Finalizes the data into the "card-container" div within the index.hmtml
+    document.getElementById("card-container").innerHTML = output;
+})
+}
