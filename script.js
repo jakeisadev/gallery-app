91b6e853966648407fb6382938a3e28a429415ad
jakeisadev <starowicz@protonmail.com>
2022-05-20 22:18:21 -0400

    major changes to layout

--- script.js
+++ script.js
@@ -20,41 +20,33 @@ fetch(url)
 
       //We do this by letting JS dynamically enter HTML depending on the data retrieved. 
         output += `
-        <div class="card">
-        <div id="picture">
-        <img src="${item.urls.regular}" alt="${item.description ?? ''}">
-        </div>
-        <div id="container">
-           <p>Liked By: ${item.likes}</p>
-        </div>
-      </div>`
+        <img src="${item.urls.regular}" alt="${item.description ?? ''}">`
     })
     //Finalizes the data into the "card-container" div within the index.hmtml
     document.getElementById("card-container").innerHTML = output;
 })
 }
 
-const onLoadImage = () => { 
-let url = `https://api.unsplash.com/search/photos?page=1&query=cat&client_id=_qiFC3j_JuFn4ZjO7ks1Z4h3dUDevonoxangBW2eXZo`
-fetch(url)
-.then(res => res.json())
-.then((data) => {
-    data.results.forEach((item) => {
-      //forEach item within the fetched API, output a card containing the image,
-      //description, and amount of likes that it has.
-
-      //We do this by letting JS dynamically enter HTML depending on the data retrieved. 
-        output += `
-        <div class="card">
-        <div id="picture">
-        <img src="${item.urls.regular}" alt="${item.description ?? ''}">
-        </div>
-        <div id="container">
-           <p>Liked By: ${item.likes}</p>
-        </div>
-      </div>`
-    })
-    //Finalizes the data into the "card-container" div within the index.hmtml
-    document.getElementById("card-container").innerHTML = output;
-})
-}
+const onLoadImages = (event) => { 
+  event.preventDefault();
+let data = document.getElementById('data').value;
+//Data that will be retrieved is the value returned from the input field.
+document.getElementById('data').value = '';
+  let url = `https://api.unsplash.com/search/photos?page=1&query=coffee&client_id=_qiFC3j_JuFn4ZjO7ks1Z4h3dUDevonoxangBW2eXZo`;
+  let output = '' //Variable to hold the dynamic HTML content
+  fetch(url)
+  .then(res => res.json())
+  .then((data) => {
+      data.results.forEach((item) => {
+        console.log(data);
+        //forEach item within the fetched API, output a card containing the image,
+        //description, and amount of likes that it has.
+  
+        //We do this by letting JS dynamically enter HTML depending on the data retrieved. 
+          output += `
+          <img src="${item.urls.regular}" alt="${item.description ?? ''}">`
+      })
+      //Finalizes the data into the "card-container" div within the index.hmtml
+      document.getElementById("card-container").innerHTML = output;
+  })
+  }
